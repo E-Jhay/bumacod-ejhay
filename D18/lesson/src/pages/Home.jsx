6e442bd7aecb9c82a225202617566b747c5ef815
ejhaybumacod <bumacodejhay@gmail.com>
2024-12-02 19:32:00 +0800

    Added D18 lesson

---
+++ D18/lesson/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-center mt-5">
+      <h1>Welcome to Movie Finder</h1>
+      <p>Search and discover your favorite movies!</p>
+    </div>
+  );
+};
+
+export default Home;
